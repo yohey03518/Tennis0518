f3a380855cf9cdb4ae7caba76e822bd7adb0c112
yarin <yohey03518@hotmail.com>
2019-05-18 22:48:42 +0800

    Love All

---
+++ Tennis0518/Tennis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tennis0518
+{
+    internal class Tennis
+    {
+        private string player1;
+        private string player2;
+
+        public Tennis(string player1, string player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        internal string ShowPoint()
+        {
+            return "Love All";
+        }
+    }
+}
---
+++ Tennis0518/TennisUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Tennis0518
+{
+    [TestFixture]
+    public class TennisUnitTest
+    {
+        [TestCase]
+        public void LoveAll()
+        {
+            string player1 = "Jay";
+            string player2 = "Moon";
+            Tennis game = new Tennis(player1, player2);
+            string result = game.ShowPoint();
+            Assert.AreEqual("Love All", result);
+        }
+    }
+}
